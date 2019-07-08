49c605871c11f98958f04c5de10cfa4079bef00b
Jengopockets <jengopockets@gmail.com>
2019-07-08 19:13:32 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-  const [homeScore, setHomeScore] = useState(7);
+  const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
   return (
@@ -32,7 +32,7 @@ function App() {
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => setHomeScore( homeScore+7 )}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal onClick={() => setHomeScore( homeScore+3 )}">Home Field Goal</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore( homeScore+3 )}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => setAwayScore( awayScore+7 )}>Away Touchdown</button>
